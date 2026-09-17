0a4c019d4becca8f82ba98a3aeb7fe72f5c167ef
MiguelBeloDuarte <miguelbeloduarte7@gmail.com>
2026-09-17 10:59:15 +0000

    aula2

--- aula2.sql
+++ aula2.sql
@@ -1,2 +1,9 @@
-SET IDENTITY_INSERT production.categories ON;
-INSERT INTO production.categories(category_id,category_name) VALUES (8,'Hybrid Bikes')
+SELECT * FROM sales.stores
+SELECT brand_name FROM production.brands
+SELECT customer_id, first_name, last_name FROM sales.customers WHERE city = 'Houston'
+SELECT * FROM sales.customers WHERE state = 'TX' OR state = 'CA'
+SELECT * FROM production.products WHERE list_price > 1500
+SELECT product_name FROM production.products WHERE model_year > 2017 AND list_price < 1000
+SELECT DISTINCT product_name FROM production.products WHERE model_year = 2017 OR model_year = 2018 AND list_price < 1250 AND list_price > 750
+SELECT * FROM production.products WHERE list_price = (SELECT max(list_price) FROM production.products)
+SELECT list_price = avg(list_price) FROM production.products
